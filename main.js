f5741559c398f633cc56182f253a52caf86ee4ea
manit20 <78215866+manit20@users.noreply.github.com>
2021-06-15 18:40:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){}
+
+function setup(){
+    canvas = createCanvas(320, 300);
+    canvas.center();
+}
+
+function draw(){}
+
+function take_snapshot(){
+    save("clown_image.png");
+}
+
